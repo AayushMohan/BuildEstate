9dcab3907262743aaa084732fcd8b40f248f8faa
AayushMohan <aayushmohan1702@gmail.com>
2022-10-03 23:55:04 +0530

    Added: Header Logo

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,7 +3,11 @@ import React from "react";
 type Props = {};
 
 const Header = (props: Props) => {
-  return <div>Header</div>;
+  return (
+    <div>
+      <img src="https://img.icons8.com/nolan/64/b.png" />
+    </div>
+  );
 };
 
 export default Header;
