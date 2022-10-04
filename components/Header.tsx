c7ab3ee6f65058c19ed037a3599d42c8376fd754
AayushMohan <aayushmohan1702@gmail.com>
2022-10-04 20:38:23 +0530

    Added: Header Styles

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,23 +1,30 @@
+import Link from "next/link";
 import React from "react";
+import { motion } from "framer-motion";
 
 type Props = {};
 
 const Header = (props: Props) => {
   return (
-    <div>
-      <div>
-        <img src="https://img.icons8.com/nolan/64/b.png" />
+    <motion.div className="flex justify-around p-4 md:h-screen md:bg-gradient-to-b from-indigo-500">
+      <div className="flex text-4xl font-bold items-center">
+        <img
+          src="https://img.icons8.com/nolan/64/b.png"
+          className="h-10 w-10"
+        />
+        <h1 className="text-gray-700 md:block hidden">
+          <span className="text-indigo-600">Build</span>
+          Estate
+        </h1>
       </div>
 
-      <div>
-        <ul>
-          <li>Home</li>
-          <li>How It's Work</li>
-          <li>Agent</li>
-          <li>Sales</li>
-        </ul>
+      <div className="flex flex-col space-x-4 md:flex-row space-y-10">
+        <Link href="">Home</Link>
+        <Link href="">How It's Work</Link>
+        <Link href="">Agent</Link>
+        <Link href="">Sales</Link>
       </div>
-    </div>
+    </motion.div>
   );
 };
 
