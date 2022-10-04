f53a9357a545949fdd8520daa9caf169eafb55f0
AayushMohan <aayushmohan1702@gmail.com>
2022-10-04 21:48:51 +0530

    Finished: Header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,28 +1,40 @@
 import Link from "next/link";
 import React from "react";
 import { motion } from "framer-motion";
+import { ChevronDownIcon, PlayIcon } from "@heroicons/react/20/solid";
 
 type Props = {};
 
 const Header = (props: Props) => {
   return (
-    <motion.div className="flex justify-around p-4 md:h-screen md:bg-gradient-to-b from-indigo-500">
-      <div className="flex text-4xl font-bold items-center">
+    <motion.div className="flex justify-around p-4 m-4 items-center">
+      <div className="flex text-3xl space-x-4 font-bold items-center">
         <img
           src="https://img.icons8.com/nolan/64/b.png"
           className="h-10 w-10"
         />
-        <h1 className="text-gray-700 md:block hidden">
+        <h1 className="text-gray-700">
           <span className="text-indigo-600">Build</span>
           Estate
         </h1>
       </div>
 
-      <div className="flex flex-col space-x-4 md:flex-row space-y-10">
+      <div className="flex space-x-8 text-gray-500">
         <Link href="">Home</Link>
         <Link href="">How It's Work</Link>
         <Link href="">Agent</Link>
         <Link href="">Sales</Link>
+      </div>
+
+      <div className="flex space-x-4">
+        <button className="px-10 py-2 bg-slate-100 text-gray-500 rounded-xl relative">
+          USD
+          <PlayIcon className="rotate-90 h-3 w-3 absolute top-1/3 right-4" />
+        </button>
+
+        <button className="bg-indigo-600 text-gray-100 px-4 py-3 rounded-xl">
+          Contact Us
+        </button>
       </div>
     </motion.div>
   );
