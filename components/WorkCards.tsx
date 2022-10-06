ca4431ea07234a5cbc5040c760e2a4486b6f87bc
AayushMohan <aayushmohan1702@gmail.com>
2022-10-06 23:24:42 +0530

    Completed: WorkCards

--- components/WorkCards.tsx
+++ components/WorkCards.tsx
@@ -5,10 +5,49 @@ type Props = {};
 const WorkCards = (props: Props) => {
   return (
     <div className="mt-14 -mx-14 flex justify-around">
-      <div className="h-80 w-72 shadow-md rounded-lg bg-white"></div>
-      <div className="h-80 w-72 shadow-md rounded-lg bg-white"></div>
-      <div className="h-80 w-72 shadow-md rounded-lg bg-white"></div>
-      <div className="h-80 w-72 shadow-md rounded-lg bg-white"></div>
+      <div className="default-card">
+        <div className=" bg-violet-200 rounded-full w-16 flex p-4 my-8">
+          <img src="https://img.icons8.com/sf-regular-filled/48/4338ca/topic.png" />
+        </div>
+        <h1 className="text-2xl font-medium text-slate-500">Communication</h1>
+        <p className="text-sm mx-12 text-center my-2 text-slate-500/80 font-medium">
+          Single point of contact, in sync with the tools you use, We speak your
+          language.
+        </p>
+      </div>
+
+      <div className="default-card">
+        <div className=" bg-violet-200 rounded-full w-16 flex p-4 my-8">
+          <img src="https://img.icons8.com/sf-regular-filled/48/4338ca/keyhole-shield.png" />
+        </div>
+        <h1 className="text-2xl font-medium text-slate-500">Reliability</h1>
+        <p className="text-sm mx-12 text-center my-2 text-slate-500/80 font-medium">
+          We keep our promises, provide upfront timelines and bring
+          predictability
+        </p>
+      </div>
+
+      <div className="default-card">
+        <div className=" bg-violet-200 rounded-full w-16 flex p-4 my-8">
+          <img src="https://img.icons8.com/ios-filled/48/4338ca/fantasy.png" />
+        </div>
+        <h1 className="text-2xl font-medium text-slate-500">Quality First</h1>
+        <p className="text-sm mx-12 text-center my-2 text-slate-500/80 font-medium">
+          Quality First. All projects are backed by our fanatic support & 100%
+          satisfaction guaranteed.
+        </p>
+      </div>
+
+      <div className="default-card">
+        <div className=" bg-violet-200 rounded-full w-16 flex p-4 my-8">
+          <img src="https://img.icons8.com/material-rounded/48/4338ca/user-location.png" />
+        </div>
+        <h1 className="text-2xl font-medium text-slate-500">Families</h1>
+        <p className="text-sm mx-12 text-center my-2 text-slate-500/80 font-medium">
+          Rent a beautiful house for your family. Choose from flats in societies
+          and invalid apartments
+        </p>
+      </div>
     </div>
   );
 };
