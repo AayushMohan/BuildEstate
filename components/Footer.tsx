00ed0509832c4aa52e38d13883c4d0c7c89a1784
AayushMohan <aayushmohan1702@gmail.com>
2022-10-10 15:22:38 +0530

    Added: Styles in Footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,15 +6,19 @@ type Props = {};
 
 const Footer = (props: Props) => {
   return (
-    <div className="h-80">
-      <hr className="" />
-      <div className="flex text-3xl space-x-4 font-bold items-center">
+    <div className="h-80 relative">
+      <div className="border border-b-1" />
+      <div className="flex text-3xl space-x-4 font-bold items-center p-10 mx-14">
         <Image src={logo} className="" />
         <h1 className="text-gray-700">
           <span className="text-indigo-600">Build</span>
           Estate
         </h1>
+        <p className="text-base font-normal w-[300px] p-6">
+          The shortest distance between paradise and the place you call home.
+        </p>
       </div>
+      <div className="border border-b-1 w-[315px] rotate-90 absolute bottom-1/2 right-[600px] " />
     </div>
   );
 };
